93b94625511e19a923205301a5f4e02184da8082
shenghaoc <shenghaoc@outlook.com>
2022-02-17 17:38:05 +0800

    Adjust chart params

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ export const options = {
     },
     title: {
       display: true,
-      text: 'Chart.js Line Chart',
+      text: 'Predictions for Next 12 Months',
     },
   },
 };
@@ -93,17 +93,11 @@ export function App() {
     labels,
     datasets: [
       {
-        label: 'Dataset 1',
-        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        label: 'Sample Predictions',
+        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
         borderColor: 'rgb(255, 99, 132)',
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      {
-        label: 'Dataset 2',
-        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-      },
+      }
     ],
   });
 
@@ -128,10 +122,10 @@ export function App() {
           labels: data.result.records.map((record: { month: any; }) => record.month),
           datasets: [
             {
-              label: 'Test',
+              label: 'Predictions',
               data: data.result.records.map((record: { resale_price: any; }) => record.resale_price),
-              borderColor: 'rgb(0, 0, 0)',
-              backgroundColor: 'rgba(1, 2, 132, 0.5)',
+              borderColor: 'rgb(53, 162, 235)',
+              backgroundColor: 'rgba(53, 162, 235, 0.5)',
             },
           ],
         })
