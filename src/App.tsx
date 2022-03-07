8e5031e120ad1e525a3da961d13952419ba827b8
shenghaoc <shenghaoc@outlook.com>
2022-03-07 08:39:41 +0800

    Update chart API

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title as chartTitle,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import { Line } from 'react-chartjs-2';
+import {Line} from '@ant-design/charts';
 import $ from 'jquery';
 import {
   Form,
@@ -22,36 +12,27 @@ import {
   Col,
   Row
 } from 'antd';
-import { DatePicker } from './components';
-import dayjs, { Dayjs } from 'dayjs';
+import {DatePicker} from './components';
+import dayjs, {Dayjs} from 'dayjs';
 
-import url_map from'./url.json'
+import url_map from './url.json'
 import town_list from './town.json';
 import flat_type_list from './flat_type.json';
 import storey_range_list from './storey_range.json';
 import flat_model_list from './flat_model.json';
 
 import customParseFormat from 'dayjs/plugin/customParseFormat';
+
 dayjs.extend(customParseFormat)
 
-const { Option } = Select;
-const { Title } = Typography;
+const {Option} = Select;
+const {Title} = Typography;
 
 const ml_model_list = Object.keys(url_map).sort()
 town_list.sort()
 flat_type_list.sort()
 storey_range_list.sort()
 flat_model_list.sort()
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  chartTitle,
-  Tooltip,
-  Legend
-);
 
 export const options = {
   responsive: true,
@@ -84,25 +65,31 @@ export function App() {
     resale_price: '',
   });
 
-  const [leaseCommenceDate, setLeaseCommenceDate] = useState< Dayjs | null>(null);
+  const [leaseCommenceDate, setLeaseCommenceDate] = useState<Dayjs | null>(null);
 
 
   const handleChange = (prop: any) => (value: any) => {
-    setValues({ ...values, [prop]: value });
+    setValues({...values, [prop]: value});
   };
 
   const [output, setOutput] = useState(0)
 
-  const [data, setData] = useState({
-    labels,
-    datasets: [
-      {
-        label: 'Sample Trends',
-        data: labels.map(() => Math.random() * 100000),
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      }
-    ],
+  // @ts-ignore
+  const obj = [{'month': labels[0], 'value': Math.random() * 100000}];
+  for (let i = 1; i < labels.length; i++) {
+    // @ts-ignore
+    obj.push({'month': labels[i], 'value': Math.random() * 100000})
+  }
+
+  const [config, setConfig] = useState({
+    data: obj,
+    height: 400,
+    xField: 'month',
+    yField: 'value',
+    point: {
+      size: 5,
+      shape: 'diamond',
+    },
   });
 
   function funPredict() {
@@ -142,130 +129,134 @@ export function App() {
 
     for (let i = 0; i <= 12; i++) {
       let tmp = [i === 12 ? curr.format('YYYY-MM') : labels[i], values.town, values.storey_range, Number(values.floor_area_sqm),
-          values.flat_model, leaseCommenceDate.year()]
+        values.flat_model, leaseCommenceDate.year()]
       if (i === 0) {
         // @ts-ignore
         INPUT_DATA_FILE["data"] = [tmp]
-      } else
-        { // @ts-ignore
-          INPUT_DATA_FILE["data"].push(tmp)
-        }
+      } else { // @ts-ignore
+        INPUT_DATA_FILE["data"].push(tmp)
+      }
     }
 
+    // @ts-ignore
+    $.ajax({
       // @ts-ignore
-      $.ajax({
+      url: url_map[values.ml_model],
+      type: "POST",
+      data: JSON.stringify(INPUT_DATA_FILE),
+      success: function (data) {
+        console.log("Data Loaded: " + JSON.stringify(data));
+        setOutput(data["predict"].pop())
         // @ts-ignore
-        url: url_map[values.ml_model],
-        type: "POST",
-        data: JSON.stringify(INPUT_DATA_FILE),
-        success: function (data) {
-          console.log("Data Loaded: " + JSON.stringify(data));
-          setOutput(data["predict"].pop())
-          setData({
-            labels: labels,
-            datasets: [
-                {
-                  label: 'Trends',
-                  data: data["predict"],
-                  borderColor: 'rgb(53, 162, 235)',
-                  backgroundColor: 'rgba(53, 162, 235, 0.5)',
-                },
-            ],
-          })
-        },
-        contentType: "application/json"
-      });
+        const obj = [{'month': labels[0], 'value': data["predict"][0]}];
+        for (let i = 1; i < labels.length; i++) {
+          // @ts-ignore
+          obj.push({'month': labels[i], 'value': data["predict"][i]})
+        }
+        setConfig({
+          data: obj,
+          height: 400,
+          xField: 'month',
+          yField: 'value',
+          point: {
+            size: 5,
+            shape: 'diamond',
+          },
+        })
+      },
+      contentType: "application/json"
+    });
   }
 
   return (
-      <>
-    <Form>
-      <Title level={2}>
-        Price Prediction
-      </Title>
-          <Form.Item
-              label="ML Model"
-          >
-            <Select defaultValue={values.ml_model} onChange={handleChange('ml_model')}>
-              {ml_model_list.map((ml_model: string) => (
-                  <Option
-                      key={ml_model}
-                      value={ml_model}
-                  >
-                    {ml_model}
-                  </Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <Form.Item
-              label="Town"
-          >
-            <Select value={values.town} onChange={handleChange('town')}>
-              {town_list.map((town: string) => (
-                <Option
-                  key={town}
-                  value={town}
-                >
-                  {town}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <Form.Item
-              label="Flat Type"
-          >
-            <Select value={values.flat_type} onChange={handleChange('flat_type')}>
-              {flat_type_list.map((flat_type: string) => (
-                <Option
-                  key={flat_type}
-                  value={flat_type}
-                >
-                  {flat_type}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <Form.Item
-          label = "Storey Range"
-          >
-            <Select value={values.storey_range} onChange={handleChange('storey_range')}>
-              {storey_range_list.map((storey_range: string) => (
-                <Option
-                  key={storey_range}
-                  value={storey_range}
-                >
-                  {storey_range}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <Form.Item
-          label= "Flat Model"
-          >
-            <Select value={values.flat_model} onChange={handleChange('flat_model')}>
-              {flat_model_list.map((flat_model: string) => (
-                <Option
-                  key={flat_model}
-                  value={flat_model}
-                >
-                  {flat_model}
-                </Option>
-              ))}
-            </Select>
-          </Form.Item>
-          <Form.Item
-             label = "Floor Area"
-          >
-            <InputNumber
-              type = "number"
-              min = "0"
-              onChange={handleChange('floor_area_sqm')}
-              addonAfter = "m²"
-            />
-          </Form.Item>
-          <Form.Item
-              label = "Lease Commence Date"
-          >
+    <>
+      <Form>
+        <Title level={2}>
+          Price Prediction
+        </Title>
+        <Form.Item
+          label="ML Model"
+        >
+          <Select defaultValue={values.ml_model} onChange={handleChange('ml_model')}>
+            {ml_model_list.map((ml_model: string) => (
+              <Option
+                key={ml_model}
+                value={ml_model}
+              >
+                {ml_model}
+              </Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Town"
+        >
+          <Select value={values.town} onChange={handleChange('town')}>
+            {town_list.map((town: string) => (
+              <Option
+                key={town}
+                value={town}
+              >
+                {town}
+              </Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Flat Type"
+        >
+          <Select value={values.flat_type} onChange={handleChange('flat_type')}>
+            {flat_type_list.map((flat_type: string) => (
+              <Option
+                key={flat_type}
+                value={flat_type}
+              >
+                {flat_type}
+              </Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Storey Range"
+        >
+          <Select value={values.storey_range} onChange={handleChange('storey_range')}>
+            {storey_range_list.map((storey_range: string) => (
+              <Option
+                key={storey_range}
+                value={storey_range}
+              >
+                {storey_range}
+              </Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Flat Model"
+        >
+          <Select value={values.flat_model} onChange={handleChange('flat_model')}>
+            {flat_model_list.map((flat_model: string) => (
+              <Option
+                key={flat_model}
+                value={flat_model}
+              >
+                {flat_model}
+              </Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Floor Area"
+        >
+          <InputNumber
+            type="number"
+            min="0"
+            onChange={handleChange('floor_area_sqm')}
+            addonAfter="m²"
+          />
+        </Form.Item>
+        <Form.Item
+          label="Lease Commence Date"
+        >
           <DatePicker
             picker="year"
             value={leaseCommenceDate}
@@ -273,19 +264,22 @@ export function App() {
               setLeaseCommenceDate(newValue);
             }}
           />
-          </Form.Item>
-      <Row gutter={16}>
-        <Col span={12}>
-          <Statistic title="Prediction" value={output} prefix="$" precision={2} />
-          <Button style={{ marginTop: 16 }} type="primary"
-                  onClick={funPredict}>
-            Get prediction
-          </Button>
-        </Col>
-      </Row>,
-    </Form>
-        <Line options={options} data={data} />
-      </>
+        </Form.Item>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Statistic title="Prediction" value={output} prefix="$" precision={2}/>
+            <Button style={{marginTop: 16}} type="primary"
+                    onClick={funPredict}>
+              Get prediction
+            </Button>
+          </Col>
+        </Row>,
+      </Form>
+      <Title level={3}>
+        Predicted Trends for Past 12 Months
+      </Title>
+      <Line {...config} />
+    </>
   )
 }
 
