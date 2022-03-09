a365c4d05b954ba6a2394f488026f7c1f1031e49
shenghaoc <shenghaoc@outlook.com>
2022-03-09 12:25:19 +0800

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,25 @@ module.exports = {
   },
     plugins: [
         {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `GatsbyJS`,
+                short_name: `GatsbyJS`,
+                start_url: `/`,
+                background_color: `#f7f0eb`,
+                theme_color: `#a2466c`,
+                display: `standalone`,
+                icon: `src/images/icon.png`
+            },
+        },
+        {
             resolve: `gatsby-plugin-less`,
             options: {
                 lessOptions: {
                     javascriptEnabled: true,
                 }
             }
-        },
-    ]
+        }
+    ],
+    pathPrefix: "/prediction-tool"
 }
