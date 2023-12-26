0d530a76a095e447d9bfe567f73ea37bc690a15e
shenghaoc <shenghaoc@outlook.com>
2023-12-27 02:07:29 +0800

    Use App Router

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import '../styles/globals.css'
+
+function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
+
+export default RootLayout
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
