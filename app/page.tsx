71d015cf2fed03752454d776a4f895c2b648ac4c
shenghaoc <shenghaoc@outlook.com>
2023-12-27 03:14:32 +0800

    Update organization

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 import React, {useState} from 'react';
 import dynamic from 'next/dynamic'
