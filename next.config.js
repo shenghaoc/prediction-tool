11593ea6012151c2e10acce4260150d898aaca9d
shenghaoc <shenghaoc@outlook.com>
2023-12-26 20:04:14 +0800

    Resolve CommonJS and ES module issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: [
+    "@ant-design",
+    "rc-picker",
+    "rc-util",
+    "rc-pagination"
+  ],
+  experimental: {
+    esmExternals: 'loose',
+  },
 }
-
-module.exports = nextConfig
+ 
+export default nextConfig
