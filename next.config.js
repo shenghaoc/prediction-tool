d8608171327387506547856e032211942287d464
shenghaoc <shenghaoc@outlook.com>
2023-12-30 11:42:25 +0800

    Remove outdated config values

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: [
-    "@ant-design",
-    "rc-picker",
-    "rc-util",
-    "rc-pagination"
-  ],
-  experimental: {
-    esmExternals: 'loose',
-  },
 }
  
 export default nextConfig
