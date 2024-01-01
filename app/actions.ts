e46ed0ee726daf4d55f0276ec2c7cf7f012fbb77
shenghaoc <shenghaoc@outlook.com>
2024-01-01 13:35:27 +0800

    Restore validation

--- app/actions.ts
+++ app/actions.ts
@@ -13,39 +13,43 @@ dayjs.extend(customParseFormat)
 dayjs.extend(utc)
 
 let curr = dayjs.utc("2022-02", "YYYY-MM")
+const ml_model_map_promise: Promise<any[]> = prisma.$queryRaw`SELECT ml_models.name, intercept_map, month_map, 
+storey_range_map, floor_area_sqm_map, lease_commence_date_map,
+towns_onehot.name 
+AS town_name, 
+towns_onehot.value 
+AS town_map, 
+flat_models_onehot.name 
+AS flat_model_name, 
+flat_models_onehot.value 
+AS flat_model_map 
+FROM ((ml_models 
+JOIN towns_onehot 
+ON ml_models.name=towns_onehot.ml_model) 
+JOIN flat_models_onehot 
+ON ml_models.name=flat_models_onehot.ml_model);`
 const month_map_promise: Promise<any[]> = prisma.$queryRaw`SELECT name, value
 FROM months_ordinal;`
 const storey_range_map_promise: Promise<any[]> = prisma.$queryRaw`SELECT name, value
 FROM storey_ranges_ordinal;`
-// const schema = z.object({
-//     ml_model: z.custom<string>((val) => Object.keys(ml_model_map).includes(val as string)),
-//     town: z.custom<string>((val) => town_list.includes(val as string)),
-//     storey_range: z.custom<string>((val) => Object.keys(storey_range_map).includes(val as string)),
-//     flat_model: z.custom<string>((val) => flat_model_list.includes(val as string)),
-//     floor_area_sqm: z.number().positive('Must be greater than 0'),
-//     lease_commence_date: z.coerce.date().min(new Date("1960-01-01"), { message: "Must not be before 1960" })
-//         .max(curr.toDate(), { message: "Must not be in future" })
-// })
+const schema = z.object({
+    ml_model: z.string(),
+    town: z.string(),
+    storey_range: z.string(),
+    flat_model: z.string(),
+    floor_area_sqm: z.number().positive('Must be greater than 0'),
+    lease_commence_date: z.coerce.date().min(new Date("1960-01-01"), { message: "Must not be before 1960" })
+        .max(curr.toDate(), { message: "Must not be in future" })
+})
 
-// type Schema = z.output<typeof schema>
-export const funPredict = cache(async (values: any) => {
-    const parsed = values
+type Schema = z.output<typeof schema>
+export const funPredict = cache(async (values: Schema) => {
+    const parsed = schema.parse(values)
 
-    const result = await prisma.$queryRaw`SELECT intercept_map, month_map, 
-    storey_range_map, floor_area_sqm_map, lease_commence_date_map,
-    towns_onehot.value 
-    AS town_map, 
-    flat_models_onehot.value 
-    AS flat_model_map 
-    FROM ((ml_models 
-    JOIN towns_onehot 
-    ON ml_models.name=towns_onehot.ml_model) 
-    JOIN flat_models_onehot 
-    ON ml_models.name=flat_models_onehot.ml_model) 
-    WHERE ml_models.name=${parsed.ml_model} 
-    AND towns_onehot.name=${parsed.town}
-    AND flat_models_onehot.name=${parsed.flat_model};`
-    const full_map = (result as any[]).find((element) => element != undefined);
+    const full_map = (await ml_model_map_promise)
+        .find((element) => element.name == parsed.ml_model
+            && element.town_name == parsed.town
+            && element.flat_model_name == parsed.flat_model);
     const month_map = await month_map_promise;
     const storey_range_map = await storey_range_map_promise;
 
