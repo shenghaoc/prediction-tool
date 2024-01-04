acadb76e6a31f608dd882dc28b17ff238d295f95
shenghaoc <shenghaoc@outlook.com>
2024-01-05 03:14:57 +0800

    Use AntdRegistry

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import StyledJsxRegistry from './registry';
-
-const inter = Inter({ subsets: ['latin'] });
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 
 export const metadata: Metadata = {
 	title: 'Prediction Tool',
@@ -16,8 +13,8 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>
-				<StyledJsxRegistry>{children}</StyledJsxRegistry>
+			<body>
+				<AntdRegistry>{children}</AntdRegistry>
 			</body>
 		</html>
 	);
